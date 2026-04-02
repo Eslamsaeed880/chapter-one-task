0c1dca1b96b28eda5c51f311902c770492188128
Eslam Saeed <eslamsaeed298@gmail.com>
2026-04-02 07:51:24 +0200

    Add AppHeader component with styles and icon

---
+++ my-app/src/components/AppHeader.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+export const AppHeader: React.FC = () => {
+  return (
+    <View style={styles.header}>
+      <FontAwesome5 name="tasks" size={29} color="black" />
+      <Text style={styles.title}>Task Manager</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#ffffff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+});
